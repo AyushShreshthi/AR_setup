23144980dd882111834f9d131182b1f9fffdd744
ayush <ayushshreshthi41@gmail.com>
2022-02-11 14:16:41 +0530

    adding

---
+++ Working_With_Video_and_Audio/Assets/Scripts/AR_placeobjects.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class AR_placeobjects : MonoBehaviour
+{
+    public GameObject prefab;
+
+    private GameObject spawnObject;
+    private ARRaycastManager arRaycastmanager;
+    private Vector2 touchPosition;
+
+    static List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    private void Awake()
+    {
+        arRaycastmanager = GetComponent<ARRaycastManager>();
+    }
+
+    bool tryGetTouchPosition(out Vector2 touchPosition)
+    {
+        if (Input.touchCount > 0)
+        {
+            touchPosition = Input.GetTouch(index: 0).position;
+            return true;
+        }
+
+        touchPosition = default;
+
+        return false;
+    }
+
+    private void Update()
+    {
+        if(!tryGetTouchPosition(out Vector2 touchPosition))
+        {
+            return;
+        }
+
+        if (arRaycastmanager.Raycast(screenPoint: touchPosition, hits, TrackableType.PlaneWithinPolygon))
+        {
+            var hitPose = hits[0].pose;
+
+            if (spawnObject == null)
+            {
+                spawnObject = Instantiate(prefab, hitPose.position, hitPose.rotation);
+            }
+            else
+            {
+                spawnObject.transform.position = hitPose.position;
+            }
+        }
+    }
+
+}
---
+++ Working_With_Video_and_Audio/Assets/Scripts/ImageRecognition.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.ARFoundation;
+
+
+public class ImageRecognition : MonoBehaviour
+{
+    private ARTrackedImageManager _arTrackedImagemanager;
+
+    private void Awake()
+    {
+        _arTrackedImagemanager = FindObjectOfType<ARTrackedImageManager>();
+    }
+
+    public void OnEnable()
+    {
+        _arTrackedImagemanager.trackedImagesChanged += OnImageChanged;
+    }
+    private void OnDisable()
+    {
+        _arTrackedImagemanager.trackedImagesChanged -= OnImageChanged;
+
+    }
+    private void OnImageChanged(ARTrackedImagesChangedEventArgs args)
+    {
+        foreach (var trackedImage in args.added)
+        {
+            Debug.Log(trackedImage.name);
+        }
+    }
+}
